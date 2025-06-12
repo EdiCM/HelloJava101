d7d109ebf10a479f04a189b9c84b6dc504a0609f
Edith <edi.came040504@gmail.com>
2025-06-12 15:16:28 -0600

    fourth_2 commit

---
+++ src/c01_beginner/BeginnerExercises.java
@@ -0,0 +1,51 @@
+package c01_beginner;
+
+public class BeginnerExercises {
+    public static void main(String[] args) {
+        // 1. Declara una variable de tipo String y asígnale tu nombre.
+        String myName = "Edith";
+        System.out.println(myName);
+
+        // 2. Crea una variable de tipo int y asígnale tu edad.
+        int myAge = 21;
+        System.out.println(myAge);
+
+        // 3. Crea una variable double con tu altura en metros.
+        double myHeight = 1.68;
+        System.out.println(myHeight);
+
+        // 4. Declara una variable de tipo boolean que indique si te gusta programar.
+        boolean likeProgramming = true;
+        System.out.println(likeProgramming);
+
+        // 5. Declara una constante con tu email.
+        final String myEmail= "edi.came040504@gmail.com";
+        System.out.println(myEmail);
+
+        // 6. Crea una variable de tipo char y guárdale tu inicial.
+        char myInitial = 'E';
+        System.out.println(myInitial);
+
+        // 7. Declara una variable de tipo String con tu localidad, y a continuación cambia su valor y vuelve a imprimirla.
+        String myCountry = "Mexico";
+        System.out.println(myCountry);
+        myCountry = "Thailand";
+        System.out.println(myCountry);
+
+        // 8. Crea una variable int llamada a, otra b, e imprime la suma de ambas.
+        int a = 100;
+        int b = 1;
+        System.out.println(a + b);
+
+        // 9. Imprime el tipo de dos variables creadas anteriormente.
+        System.out.println(((Object)a).getClass().getSimpleName()); // como esta es primitiva, lo que hice es convertirlo en un objeto temporalmente obligando a Java a realizar el autoboxing de mi int a la clase Integer, pudiendo usar el metodo antes aprendido
+        System.out.println(myCountry.getClass().getSimpleName()); // este si es clase entonces nomas use los metodos, no tuve que convertir nada
+
+        // 10. Intenta declarar una variable sin inicializarla y luego asígnale un valor antes de imprimirla.
+        int c;
+        c = 3; //Esto si se puede, pero si no le asignamos ningun valor en ningun momento eso seria un error
+        System.out.println(c);
+    }
+    }
+
+
---
+++ src/c02_operators/Operators.java
@@ -0,0 +1,82 @@
+package c02_operators;
+
+public class Operators {
+    // Operadores
+    public static void main(String[] args) {
+        // Aritméticos
+
+        var a = 5;
+        var b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // Asignación
+
+        a = b;
+        System.out.println(a);
+
+        a = b * 2;
+        System.out.println(a);
+
+        a += 1; // a = a + 1
+        System.out.println(a);
+
+        a -= 1;
+        System.out.println(a);
+        a *= 2;
+        System.out.println(a);
+        a /= 2;
+        System.out.println(a);
+        a %= 2;
+        System.out.println(a);
+
+        // Comparación (Relacionales)
+
+        System.out.println(a == b);
+        System.out.println(a == 0);
+
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+
+        // Lógicos
+
+        // && o Y (AND)
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+
+        System.out.println(3 > 2 && 5 == 2);
+
+        // O o || (OR)
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+
+        System.out.println(3 > 2 || 5 == 2);
+
+        // NO o ! (NOT)
+        System.out.println(!true);
+        System.out.println(!false);
+
+        System.out.println(!(3 > 2) || 5 == 2);
+
+        // Unarios
+        System.out.println(+b);
+        System.out.println(-b);
+        System.out.println(++b);
+        System.out.println(b++);
+        System.out.println(b);
+        System.out.println(--b);
+        System.out.println(b--);
+        System.out.println(b);
+    }
+}
