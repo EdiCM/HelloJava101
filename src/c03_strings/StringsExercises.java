6102d08dac5a9885c0939b4a353c86caefc2fc7e
Edith <edi.came040504@gmail.com>
2025-06-12 19:31:17 -0600

    fourth_4 commit

--- src/c03_strings/StringsExercises.java
+++ src/c03_strings/StringsExercises.java
@@ -3,23 +3,31 @@ package c03_strings;
 public class StringsExercises {
     public static void main(String[] args) {
         // 1. Concatena dos cadenas de texto.
+        String name = "Edith";
+        String surname = "Cabral";
+        String phrase = "Hola, me gusta codeforces";
+        int age = 21;
 
+        System.out.println(name + surname);
         // 2. Muestra la longitud de una cadena de texto.
-
+        System.out.println(name.length());
         // 3. Muestra el primer y último carácter de un string.
-
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(name.length() - 1));
         // 4. Convierte a mayúsculas y minúsculas un string.
-
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
         // 5. Comprueba si una cadena de texto contiene una palabra concreta.
-
+        System.out.println(phrase.contains("gusta"));
         // 6. Formatea un string con un entero.
-
+        System.out.println(String.format("Hola, soy %s, y mi edad es %d", name, age));
         // 7. Elimina los espacios en blanco al principio y final de un string.
-
+        System.out.println(" Hola, me llamo Edith ".trim());
         // 8. Sustituye todos los espacios en blanco de un string por un guión (-).
-
+        System.out.println(" Hola, me llamo Edith ".replace(" ", "-"));
         // 9. Comprueba si dos strings son iguales.
-
+        System.out.println(name.equals(surname));
         // 10. Comprueba si dos strings tienen la misma longitud.
+        System.out.println(name.length() == surname.length()); // Aqui si usamos == en vez de equals porque equals es para el contenido
     }
 }
--- src/c04_conditionals/ConditionalsExercises.java
+++ src/c04_conditionals/ConditionalsExercises.java
@@ -1,23 +1,122 @@
 package c04_conditionals;
 
 public class ConditionalsExercises {
-    // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
+    public static void main(String[] args) {
+        // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("El usuario puede votar");
+        }
+        else {
+            System.out.println("El usuario no puede votar");
+        }
+        // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
+        int a = 20;
+        int b = 22;
+        if (a > b) {
+            System.out.println("a es mayor que b");
+        }
+        else if (a < b) {
+            System.out.println("a es menor que b");
+        }
+        else {
+            System.out.println("Son iguales");
+        }
+        // 3. Dado un número, verifica si es positivo, negativo o cero.
+        int c = -15;
+        if (c > 0) {
+            System.out.println("Es positivo");
+        }
+        else if(c == 0) {
+            System.out.println("Es cero");
+        }
+        else {
+            System.out.println("Es negativo");
+        }
+        // 4. Crea un programa que diga si un número es par o impar.
+        int d = 20;
+        if (d % 2 == 0){
+            System.out.println("Es par");
+        }
+        else {
+            System.out.println("Es impar");
+        }
+        // 5. Verifica si un número está en el rango de 1 a 100.
+        int e = 99;
+        if (e >= 1 && e <= 100){
+            System.out.println("Esta en el rango de 1 a 100");
+        }
+        // 6. Declara una variable con el día de la semana (1-7) y muestra su nombre con switch.
+        int day = 5;
+        switch (day){
+            case 1:
+                System.out.println("Es lunes");
+                break;
+            case 2:
+                System.out.println("Es martes");
+                break;
+            case 3:
+                System.out.println("Es miercoles");
+                break;
+            case 4:
+                System.out.println("Es jueves");
+                break;
+            case 5:
+                System.out.println("Es viernes");
+                break;
+            case 6:
+                System.out.println("Es sabado");
+                break;
+            case 7:
+                System.out.println("Es domingo");
+                break;
+            default:
+                System.out.println("No corresponde a ningun dia de la semana");
+        }
+        // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o "Suspenso" según la nota (0-100).
+        int grade = 67;
+        if (grade < 60) {
+            System.out.println("Suspenso");
+        }
+        else if (grade >= 60 && grade < 85){
+            System.out.println("Aprobado");
+        }
+        else {
+            System.out.println("Sobresaliente");
+        }
+        // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al menos 15 años o ir acompañado.
+        boolean company = false;
+        int myAge = 15;
+        if (myAge >= 15 || company){
+            System.out.println("Si puede entrar al cine");
+        }
+        else {
+            System.out.println("No puede entrar");
+        }
+        // 9. Crea un programa que diga si una letra es vocal o consonante.
+        char myChar = 'e';
+        if (myChar == 'a' || myChar == 'e' || myChar == 'i' || myChar == 'o' || myChar == 'u') {
+            System.out.println("Es vocal");
+        }
+        else {
+            System.out.println("Es consonante");
+        }
+        // 10. Usa tres variables a, b, c y muestra cuál es el mayor de las tres.
+        int A = 1;
+        int B = 2;
+        int C = 3;
+        if( A > B && A > C){
+            System.out.println("A es la mayor");
+        }
+        else if ( A < B && B > C){
+            System.out.println("B es mayor");
+        }
+        else if ( C > A && C > B) {
+            System.out.println("C es mayor");
+        }
+        else {
+            System.out.println("No hay una mayor");
+        }
+    }
 
-    // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
-
-    // 3. Dado un número, verifica si es positivo, negativo o cero.
-
-    // 4. Crea un programa que diga si un número es par o impar.
-
-    // 5. Verifica si un número está en el rango de 1 a 100.
-
-    // 6. Declara una variable con el día de la semana (1-7) y muestra su nombre con switch.
-
-    // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o "Suspenso" según la nota (0-100).
-
-    // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al menos 15 años o ir acompañado.
-
-    // 9. Crea un programa que diga si una letra es vocal o consonante.
-
-    // 10. Usa tres variables a, b, c y muestra cuál es el mayor de las tres.
 }
